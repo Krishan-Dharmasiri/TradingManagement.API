409feb7ac2a6888512328db332356f524ce98512
Krishan Dharmasiri <krishantha.dharmasiri@outlook.com>
2019-03-17 17:34:03 +1100

    Added one more property to PortfolioDTO

--- TradingManagement.API/DTOs/PortfolioDTO.cs
+++ TradingManagement.API/DTOs/PortfolioDTO.cs
@@ -10,5 +10,6 @@ namespace TradingManagement.API.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
+        public decimal Return { get; set; }
     }
 }
