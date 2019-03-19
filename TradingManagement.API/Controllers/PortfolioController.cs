16e635a186b6ac860ad7a547e25dcdafa25e36da
Krishan Dharmasiri <krishantha.dharmasiri@outlook.com>
2019-03-19 21:52:48 +1100

    Added IActionResult instead of JsonResult return type for the end points so that HTTP Status code helper methods can be used

--- TradingManagement.API/Controllers/PortfolioController.cs
+++ TradingManagement.API/Controllers/PortfolioController.cs
@@ -10,15 +10,27 @@ namespace TradingManagement.API.Controllers
     public class PortfolioController:Controller
     {
         [HttpGet]
-        public JsonResult GetPortfolios()
+        public IActionResult GetPortfolios()
         {
-            return new JsonResult(TradingDataStore.Current.Portfolios);
+            //return new JsonResult(TradingDataStore.Current.Portfolios);
+            
+            /*
+             *  KD:
+             *  You can not use these HTTP Helper Status Code methods with JsonResult 
+             *  Here you don't need to check whether the collection is empty because an empty collection is also a resource
+             */
+            return Ok(TradingDataStore.Current.Portfolios);
         }
 
         [HttpGet("{id}")]
-        public JsonResult GetPortfolioById(int id)
+        public IActionResult GetPortfolioById(int id)
         {
-            return new JsonResult(TradingDataStore.Current.Portfolios.FirstOrDefault(p => p.Id == id));
+            //return new JsonResult(TradingDataStore.Current.Portfolios.FirstOrDefault(p => p.Id == id));
+            var portfolioToReturn = TradingDataStore.Current.Portfolios.FirstOrDefault(p => p.Id == id);
+            if (portfolioToReturn is null)
+                return NotFound();
+            else
+                return Ok(portfolioToReturn);
         }
 
 
